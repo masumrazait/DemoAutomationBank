e05e62831a733e7c14dec50b030d4d96ccc6b1c6
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-02-22 18:16:43 +0530

    code added

---
+++ src/test/java/coding/StringIndexSubstring.java
@@ -0,0 +1,13 @@
+package coding;
+
+public class StringIndexSubstring {
+
+	public static void main(String[] args) {
+		String input = "india";
+		String replace = "a";
+		System.out.print("a : ");
+		int index = input.indexOf(replace);
+		System.out.print(index);
+	}
+
+}
