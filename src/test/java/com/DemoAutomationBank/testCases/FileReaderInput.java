a84e136a04141782bcf88ad6706f5fad2a47b132
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-02-23 22:09:37 +0530

    code added

---
+++ src/test/java/com/DemoAutomationBank/testCases/FileReaderInput.java
@@ -0,0 +1,19 @@
+package com.DemoAutomationBank.testCases;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class FileReaderInput {
+
+	public static void main(String[] args) throws IOException {
+		String filepath = "./TestData/data.xlsx";
+		FileInputStream fis = new FileInputStream(filepath);
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		String cellvalue = sheet.getRow(0).getCell(0).getStringCellValue();
+		System.out.println(cellvalue);
+	}
+}
